525f1bb88d27230b62e62cb9e12df546ea8d06e7
kamillj <kamil.jurczak@gmail.com>
2019-02-06 20:07:15 +0100

    working servlet

--- src/HelloWorldServlet.java
+++ src/HelloWorldServlet.java
@@ -1,2 +1,14 @@
-public class HelloWorldServlet {
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorldServlet extends HttpServlet{
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        out.print("Hello Servlets");
+    }
 }

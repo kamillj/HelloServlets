109190a9a1a7d05043185926cafb55f367663c6f
kamillj <kamil.jurczak@gmail.com>
2019-02-06 20:29:26 +0100

    getting parameters from request

--- src/HelloWorldServlet.java
+++ src/HelloWorldServlet.java
@@ -9,6 +9,18 @@ public class HelloWorldServlet extends HttpServlet{
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter out = response.getWriter();
-        out.print("Hello Servlets");
+
+        String parameter1 = request.getParameter("parameter1");
+        String parameter2 = request.getParameter("parameter2");
+        String parameter3 = request.getParameter("parameter3");
+
+        out.println("Wczytanie 3 parameterow z zadania :");
+        out.println(parameter1);
+        out.println(parameter2);
+        out.println(parameter3);
+        
+        System.out.println(parameter1);
+        System.out.println(parameter2);
+        System.out.println(parameter3);
     }
 }
